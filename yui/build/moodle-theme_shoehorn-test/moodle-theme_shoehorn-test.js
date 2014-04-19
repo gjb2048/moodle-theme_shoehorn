5a4a876b03174cbcf23de90756a5a5ac873e1570
Gareth J Barnard <gjbarnard@gmail.com>
2014-04-19 17:51:32 +0100

    YUI Test.
    Conflicts:
    	lang/en/theme_shoehorn.php
    	renderers/core_renderer.php
    	settings.php
    	version.php
    	yui/build/moodle-theme_shoehorn-test/moodle-theme_shoehorn-test-debug.js
    	yui/build/moodle-theme_shoehorn-test/moodle-theme_shoehorn-test-min.js
    	yui/build/moodle-theme_shoehorn-test/moodle-theme_shoehorn-test.js
    	yui/src/test/js/test.js

---
+++ yui/build/moodle-theme_shoehorn-test/moodle-theme_shoehorn-test.js
@@ -0,0 +1,65 @@
+YUI.add('moodle-theme_shoehorn-test', function (Y, NAME) {
+
+/**
+The Shoehorn theme's test JavaScript
+
+@namespace Moodle
+@module theme_shoehorn.test
+**/
+
+/**
+@class Moodle.theme_shoehorn.test
+@uses node
+@uses selector-css3
+@constructor
+**/
+var NS = Y.namespace('Moodle.theme_shoehorn.test');
+
+/**
+ * Initialise the Moodle Bootstrap theme JavaScript
+ *
+ * @method init
+ */
+NS.init = function() {
+};
+
+var MYCLASS = function() {
+     MYCLASS.superclass.constructor.apply(this, arguments);
+};
+
+/* https://yuilibrary.com/yui/docs/yui/yui-extend.html */
+function Bird(name) {
+    this.name = name;
+}
+
+Bird.prototype.flighted   = true;  // Default for all Birds
+Bird.prototype.isFlighted = function () { return this.flighted; };
+Bird.prototype.getName    = function () { return this.name; };
+
+function Chicken(name) {
+    // Chain the constructors
+    Chicken.superclass.constructor.call(this, name);
+}
+// Chickens are birds
+Y.extend(Chicken, Bird);
+
+// Define the Chicken prototype methods/members
+//Chicken.prototype.flighted = false; // Override default for all Chickens
+
+Chicken.prototype.isFlighted = function () { return false; };
+
+function showInheritance(Y) {
+    var chicken = new Chicken('Little');
+
+
+
+
+
+    // Chicken instances inherit Bird methods and members
+
+}
+
+showInheritance(Y);
+
+
+}, '@VERSION@', {"requires": ["node", "selector-css3"]});
