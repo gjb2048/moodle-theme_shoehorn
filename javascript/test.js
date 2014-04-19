dc05855b79672e4a573bc2c5e762fa7973aa4dc6
Gareth J Barnard <gjbarnard@gmail.com>
2014-04-19 17:53:25 +0100

    V2.6.0.3

--- javascript/test.js
+++
@@ -1,3 +0,0 @@
-YUI().use('moodle-theme_shoehorn-test', function(Y) {
-    Y.Moodle.theme_shoehorn.test.init();
-});
--- yui/build/moodle-theme_shoehorn-test/moodle-theme_shoehorn-test-min.js
+++
@@ -1,1 +0,0 @@
-YUI.add("moodle-theme_shoehorn-test",function(e,t){var n=e.namespace("Moodle.theme_shoehorn.test");n.init=function(){}},"@VERSION@",{requires:["node","selector-css3"]});
