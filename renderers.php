29663b674a2393e71c8d0a44329eeb549c2b3a48
Gareth J Barnard <gjbarnard@gmail.com>
2014-07-19 20:08:31 +0100

    V2.7.0.3

--- renderers.php
+++ renderers.php
@@ -26,5 +26,5 @@
  */
 
 require_once('renderers/core_renderer.php');
+require_once('renderers/course_renderer.php');
 require_once('renderers/course_format_renderers.php');
-
--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014071500;
+$plugin->version   = 2014071900;
 $plugin->requires  = 2014051200.00; // 2.7 (Build: 20140512).
 $plugin->component = 'theme_shoehorn';
 $plugin->maturity = MATURITY_BETA;
