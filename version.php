5373d313db9678344806606484995743703ce720
Gareth J Barnard <gjbarnard@gmail.com>
2014-12-08 17:45:48 +0000

    V2.8.0.3

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014112200;
+$plugin->version   = 2014120800;
 $plugin->requires  = 2014111000.00; // 2.8 (Build: 20141110).
 $plugin->component = 'theme_shoehorn';
 $plugin->maturity = MATURITY_RC;
